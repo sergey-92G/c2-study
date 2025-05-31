070a2caaa88eaf0a3678bdd003f13ddcb78a6cf5
Sergey G.A. <gruzdsergey92@gmail.com>
2025-06-01 02:45:00 +0700

    practica and laba 1-2

---
+++ laba-1.cpp
@@ -0,0 +1,103 @@
+﻿#include <iostream>
+#include <windows.h>
+using namespace std;
+
+int FCN(string text) {
+	/*unsigned char symb = text[0];
+	unsigned char symb_A = 'А';
+	int num = symb - symb_A + 1;
+	return num;*/
+	string alf_upper = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+	string alf_lower = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+	int num;//порядковый номер первой буквы
+	bool check = false;//соответствует требованиям или нет
+
+
+
+	for (int i = 0; i < alf_upper.length(); i++) {
+		if (text[0] == alf_upper[i]) {//проверяем первая буква заглавная или нет
+			num = i + 1;//записываем порядковый номер этой буквы
+			check = true;//проверка пройдена
+			break;
+		}
+	}
+
+	if (check) {//если первая проверка пройдена
+		int kol = 0;//для записи кол-во символов в тексте кроме первой буквы
+		for (int i = 1; i < text.length(); i++) {//перебираем текст со второго символа
+			for (int j = 0; j < alf_lower.length(); j++) {//перебираем алфавит маленьких букв
+				if (text[i] == alf_lower[j]) {//если символ найден
+					kol++;//увеличиваем кол-во на один
+					break;
+				}
+			}
+		}
+		if (kol + 1 == text.length()) {//если кол-во символов совпадает
+			check = true;//записываем true то что прошло проерку
+		}
+		else {
+			check = false;//записываем false если проверку не прошло
+		}
+
+	}
+
+
+	if (check) {
+		return num;//возвращаем порядковый номер если все проверки пройдены
+	}
+	else {
+		return -1;//возвращаем -1 как ошибку
+	}
+
+
+}
+
+int LEN(string text) {
+	int size = 0;
+	for (; text[size] != '\0'; size++);
+	return size;
+}
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	string LN; //фамилия
+	string FN; //имя
+	int YY; //2 послед цифры года поступления
+	int M; //месяц поступления (2 или 9)
+	int i; //номер лабораторной работы
+	int CNT; //кол-во вариантов в лабораторной работе
+
+	cout << "Введите фамилию: ";
+	cin >> LN;
+
+	cout << "Введите имя: ";
+	cin >> FN;
+
+	cout << "Введите две последние цифры года поступления: ";
+	cin >> YY;
+
+	cout << "Введите месяц поступления (номер): ";
+	cin >> M;
+
+	cout << "Введите номер лабораторной работы: ";
+	cin >> i;
+
+	cout << "Введите кол-во вариантов в лабораторной работе: ";
+	cin >> CNT;
+
+	int FCN_LN = FCN(LN);
+	int LEN_LN = LEN(LN);
+	int FCN_FN = FCN(FN);
+	int LEN_FN = LEN(FN);
+
+	if (FCN_LN == -1 || FCN_FN == -1) {
+		cout << "Ошибка ввода данных!" << endl;
+	}
+
+	int VAR = (YY + M + FCN_LN + LEN_LN + FCN_FN + LEN_FN) * i % CNT + 1;
+	cout << "Вариант: " << VAR << endl;
+
+}
---
+++ laba-2.cpp
@@ -0,0 +1,113 @@
+﻿#include <iostream> // для std::cout, std::cin и т.д.
+#include <clocale> // для setlocale
+#include <cstdlib> // для srand и rand
+#include <vector> // для std::vector    
+#include <ctime> // для time
+using namespace std;// пространство имен для стандартной библиотеки C++
+
+void print_array(const vector <int> & arr) {
+    //функция для вывода массива на экран
+    cout << endl << "Массив: ";
+    for (const int& num : arr) {
+        cout << num << " ";
+    }
+    cout << endl;
+}
+vector <int> avto_array (int n) {
+    //функция для автоматического создания массива из случайных чисел
+    int mn;
+    int mx;
+    cout << "Нижний предел значений = "; cin >> mn; 
+    cout << "Верхний предел значений = "; cin >> mx;
+    vector <int> massive(n); // создание вектора заданного размера n
+    for (int i = 0; i < n; i++) {
+        massive[i] =mn + rand() % (mx-mn+1); // генерация случайного числа
+    }
+	return massive; // возвращаем сгенерированный массив
+}
+int ar_max(const vector <int> & arr) {
+    //функция для нахождения максимального элемента в массиве
+    int max_value = arr[0]; 
+	for (const int & num : arr) { // перебор каждого элемента массива
+        if (num > max_value) {
+            max_value = num; // обновление максимального значения
+        }
+    }
+	return max_value; // возвращаем максимальное значение
+}
+int ar_min(const vector <int> & arr) {
+    //функция для нахождения минимального элемента в массиве
+	int min_value = arr[0]; 
+    for (const int& num : arr) {
+        if (num < min_value) {
+			min_value = num; // обновление минимального значения    
+        }
+    }
+	return min_value; // возвращаем минимальное значение
+} 
+vector <int> new_massiv(const vector <int> & arr, int diff, int x) {
+    //выводит новый массив по условию 1 - больше diff; -1 - меньше diff;
+    // diff - середина max-min; x (-1 or 1) условия создания
+    vector <int> new_array;
+    if (x > 0) {
+        for (const int & num : arr) {
+            if (num > diff)
+                new_array.push_back(num); // 
+        }
+    }
+    else if (x < 0) {
+        for (const int& num : arr) {
+            if (num < diff)
+                new_array.push_back(num); // 
+        }
+    }    
+    return new_array;
+}
+int count_num(const vector <int>& arr) {
+    int count = 0;
+    for (const int& num : arr) {
+        count += 1;
+    }
+    return count;
+}
+
+int main() {
+    setlocale(LC_ALL, ""); // для корректного отображения русских символов в консоли
+	ios::sync_with_stdio(false); // отключаем синхронизацию с C-стилем ввода-вывода для ускорения работы с cin и cout
+	cin.tie(nullptr); // отключаем синхронизацию с C-стилем ввода-вывода для ускорения работы с cin и cout
+    srand(time(0)); // инициализация генератора случайных чисел
+    
+	// Начало программы
+    cout << "Введите размер массива для автогенерации N = ";
+	int n; // объявление переменной для размера массива
+    cin >> n;
+    if (n <= 0) { // проверка на корректность ввода размера массива
+        cout << "Размер массива должен быть больше 0." << endl;
+        return 1; // завершение программы с ошибкой
+	}
+	vector <int> myarray = avto_array(n); // вызов функции для автогенерации массива 
+	printf("Размер массива: %d\n", n); // вывод размера массива
+	print_array(myarray); // вывод сгенерированного массива на экран
+	
+	// Нахождение максимального и минимального элементов массива и их разницы
+	int max_value = ar_max(myarray); // вызов функции для нахождения максимального элемента
+	int min_value = ar_min(myarray); // вызов функции для нахождения минимального элемента
+    int diff = min_value + (max_value - min_value)/2;
+	cout << "Максимальный элемент массива max = " << max_value << endl; // вывод максимального элемента
+	cout << "Минимальный элемент массива min = : " << min_value << endl; // вывод минимального элемента     
+    cout << "Разница между max и min элементами, diff = " << diff << endl; // вывод разницы между максимальным и минимальным элементами
+    
+    // 2 новых массива 
+    cout << "\nНовый массив где значения меньше diff";
+    vector <int> mass1 = new_massiv(myarray, diff,-1);
+    print_array(mass1);
+    printf("Размер массива: %d\n", count_num(mass1)); // вывод размера массива
+    cout << "\nНовый массив где значения больше diff";
+    vector <int> mass2 = new_massiv(myarray, diff, 1);
+    print_array(mass2);
+    printf("Размер массива: %d\n", count_num(mass2)); // вывод размера массива
+    //
+    system("pause"); // пауза для просмотра результата в консоли
+	return 0; // успешное завершение программы
+
+}
